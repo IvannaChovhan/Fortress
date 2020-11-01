1af7dbfe4bdb4071047caacd565c8179330fe9e8
IvannaChovhan <ivanna.chovhan131@gmail.com>
2020-11-01 03:51:24 +0200

    Added Tests

--- src/com/company/exception/NotEnoughHighForTowerException.java
+++
@@ -1,7 +0,0 @@
-package com.company.exception;
-
-public class NotEnoughHighForTowerException extends Exception {
-    public NotEnoughHighForTowerException(String message) {
-        super(message);
-    }
-}
--- src/com/company/exception/NotEnoughSpaceForFortressException.java
+++
@@ -1,7 +0,0 @@
-package com.company.exception;
-
-public class NotEnoughSpaceForFortressException extends RuntimeException {
-    public NotEnoughSpaceForFortressException(String message) {
-        super(message);
-    }
-}
