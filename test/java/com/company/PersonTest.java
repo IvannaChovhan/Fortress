6b47b9dc95d0222fd5bfd8bd8438ebd34e561592
IvannaChovhan <ivanna.chovhan131@gmail.com>
2020-11-25 02:01:21 +0200

    Added unit tests

---
+++ test/java/com/company/PersonTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+    Person firstPerson = new Person("John", "Watson");
+    Person secondPerson = new Person("Mary", "Johnson");
+
+    @Test
+    public void Should_FailToCompare_WhenPersonFieldsAreDifferent() {
+        firstPerson = new Person(secondPerson);
+        Assert.assertEquals(firstPerson.getName(), secondPerson.getName());
+        Assert.assertEquals(firstPerson.getSurname(), secondPerson.getSurname());
+    }
+}
---
+++ test/java/com/company/TypeOfWarriorTest.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeOfWarriorTest {
+
+    @Test
+    public void Should_FailToDisplay_When_OutputIsIncorrect() {
+        String actual = TypeOfWarrior.showTypeOfWarriors();
+        Assert.assertEquals("archer weapon: bow; swordsman weapon: sword; spearman weapon: spear", actual);
+    }
+
+    @Test
+    public void Should_Fail_When_ToStringIsNotLowerCase() {
+        String actual = TypeOfWarrior.SWORDSMAN.toString();
+        Assert.assertEquals("swordsman", actual);
+    }
+}
